28f32f615538b8735f31292733e3ff984e38850d
Sau P <sau@yaaama.xyz>
2023-10-13 15:25:40 +0100

    change penguin into 3

--- config.h
+++ config.h
@@ -95,7 +95,7 @@ static Sp scratchpads[] = {
 };
 
 /* tagging */
-static const char *tags[] = {"", "", "", "4", "5", "6", "7", "8", "9"};
+static const char *tags[] = {"", "", "3", "4", "5", "6", "7", "8", "9"};
 
 static const Rule rules[] = {
     /* xprop(1):
